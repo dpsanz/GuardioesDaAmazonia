43eeaa4753bf7efb52a3cd310784b1d14593abc1
Reimar-Coelho <reimargamer@proton.me>
2023-10-24 21:05:55 -0300

    mudanças para adicionar mongodb e nodejs

--- script.js
+++ script.js
@@ -6,14 +6,3 @@ function index() {
     window.location.href = "index.html";
 }
 
-function checkFields() {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const loginButton = document.getElementById('loginButton');
-
-    if (email.trim() !== '' && password.trim() !== '') {
-        loginButton.disabled = false;
-    } else {
-        loginButton.disabled = true;
-    }
-}
